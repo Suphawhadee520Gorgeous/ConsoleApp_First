49fb5705e90f2110a170b5542b5cefa6a01a296b
Suphawhadee520Gorgeous <suphawhadee.monthathat2468@gmail.com>
2023-06-07 14:46:16 +0700

    Add project files.

---
+++ ConsoleApp_First/Program.cs
@@ -0,0 +1,19 @@
+﻿using  System;
+namespace ConsoleApp_First
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                int y = 0;
+            while (y < 3)
+                {
+                Console.WriteLine("While " + y);
+                y++;
+                }
+                Console.WriteLine("Loop ended"); //LOOP END
+        }
+    }
+}
+//การมีแฟนคือลาบอันประเสริฐ
+//I'am hunger.
